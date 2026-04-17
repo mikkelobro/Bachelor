a78945b3cf27f12dac59d8c2eeb1c411e4c40112
mikkelobro <mikkelobro@gmail.com>
2026-04-17 22:30:55 +0200

    tried different resolutions

--- Denoise.py
+++ Denoise.py
@@ -20,19 +20,50 @@ noise = noise_level * np.random.randn(len(x))
 x_noisy = x + noise
 
 # Apply DWT
-level = 5   # typical choice for audio
+level = 9   # typical choice for audio
 
 coeffs = pywt.wavedec(x_noisy, 'Haar', level=level)
 
-# Soft thresholding
+# Thresholding
 coeffs_thresh = [coeffs[0]]
+
+def semi_soft(d, lam1, lam2):
+    out = np.zeros_like(d)
+    
+    abs_d = np.abs(d)
+    
+    # Region 1: below lower threshold
+    mask1 = abs_d <= lam1
+    
+    # Region 2: transition
+    mask2 = (abs_d > lam1) & (abs_d < lam2)
+    
+    # Region 3: above upper threshold
+    mask3 = abs_d >= lam2
+    
+    out[mask1] = 0
+    out[mask2] = np.sign(d[mask2]) * (lam2 * (abs_d[mask2] - lam1) / (lam2 - lam1))
+    out[mask3] = d[mask3]
+    
+    return out
 
 for d in coeffs[1:]:
     sigma_j = np.median(np.abs(d)) / 0.6745
-    alpha = 0.8   # try 0.3 – 0.8
+    alpha = 1   # Threshold adjust
     lam_j = alpha * sigma_j * np.sqrt(2 * np.log(len(d)))
 
-    d_thresh = np.sign(d) * np.maximum(np.abs(d) - lam_j, 0)
+    # Hard thresholding
+    #d_thresh = d * (np.abs(d) > lam_j)
+
+    # Soft thresholding
+    #d_thresh = np.sign(d) * np.maximum(np.abs(d) - lam_j, 0)
+
+    # Semi thresholding
+    alpha = 0.5
+    lam1 = alpha * sigma_j * np.sqrt(2 * np.log(len(d)))
+    lam2 = 2 * lam1   # typical choice
+    d_thresh = semi_soft(d, lam1, lam2)
+
     coeffs_thresh.append(d_thresh)
 
 # IDWT
@@ -41,3 +72,8 @@ x_denoised = pywt.waverec(coeffs_thresh, 'haar')
 # Denoised wav file
 x_out = np.int16(x_denoised / np.max(np.abs(x_denoised)) * 32767)
 wavfile.write("denoised.wav", fs, x_out)
+
+snr_noisy = 10 * np.log10(
+    np.sum(x**2) / np.sum((x - x_noisy)**2)
+)
+print("SNR (noisy):", snr_noisy, "dB")
