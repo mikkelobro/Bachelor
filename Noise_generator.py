bd89f25d5ac58b1b6b90ab7aa79f596f7006d8e1
mikkelobro <mikkelobro@gmail.com>
2026-05-19 13:34:43 +0200

    seperate noise file

--- Noise_generator.py
+++ Noise_generator.py
@@ -22,6 +22,23 @@ x_noisy_stat = x + noise_stat
 noise_nonstat = noise_level * np.random.randn(len(x))  * (A + 1 + np.sin(f * np.pi * t))
 x_noisy_nonstat = x + noise_nonstat
 
+# Length of noise-only files in seconds
+duration = 20
+
+# Number of samples
+N = int(fs * duration)
+
+# Repeat noise if necessary
+noise_stat_long = np.tile(noise_stat, int(np.ceil(N / len(noise_stat))))[:N]
+noise_nonstat_long = np.tile(noise_nonstat, int(np.ceil(N / len(noise_nonstat))))[:N]
+
+# Save noise-only signals
+noise_out_stat = np.int16(noise_stat_long / np.max(np.abs(noise_stat_long)) * 32767)
+wavfile.write("Audio files/With noise/noise_stationary_only.wav", fs, noise_out_stat)
+
+noise_out_nonstat = np.int16(noise_nonstat_long / np.max(np.abs(noise_nonstat_long)) * 32767)
+wavfile.write("Audio files/With noise/noise_nonstationary_only.wav", fs, noise_out_nonstat)
+
 
 x_out_stat = np.int16(x_noisy_stat / np.max(np.abs(x_noisy_stat)) * 32767)
 wavfile.write("Audio files/With noise/noisy_stationary.wav", fs, x_out_stat)
